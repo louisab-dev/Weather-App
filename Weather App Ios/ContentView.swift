05724a752975c854f163a3c45b7f888077edc85f
0xjustlouis <louisamorosbessede@gmail.com>
2022-07-18 19:27:39 +0200

    Initial Commit

---
+++ Weather App Ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather App Ios
+//
+//  Created by Louis AB on 18/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather App Ios/Weather_App_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_IosApp.swift
+//  Weather App Ios
+//
+//  Created by Louis AB on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
